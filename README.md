813580ff4d5535fd7c9b17782c7c57c6bea5c37f
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2022-12-27 11:02:11 +0800

    ✨ feat: 新增内部布局

--- README.md
+++ README.md
@@ -56,11 +56,102 @@ app.mount('#app')
 
 ![image-20221227102147659](https://cdn.jsdelivr.net/gh/ranyong1997/image_collect@main/img/202212271021573.png)
 
+### 进行页面填充模块
 
+![简易版CRUD图](https://cdn.jsdelivr.net/gh/ranyong1997/image_collect@main/img/202212271058838.jpg)
 
+完整代码：
 
+```vue
+# App.vue
 
+<script setup>
+import { ref } from "vue";
+// 数据
+let queryInput = ref("")
+let tableData = ref([{
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+},
+{
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+},
+{
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+},
+{
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+}])
+// 方法
+const handleRowClick = () => {
+    console.log('click')
+}
+</script>
 
+<template>
+    <div class="table-box">
+        <!-- 标题 -->
+        <div class="title">
+            <h2>CRUD</h2>
+        </div>
+        <!-- 功能 -->
+        <div class="query-box">
+            <el-input v-model="queryInput" placeholder="请输入姓名搜索" />
+            <el-button type="primary">增加</el-button>
+        </div>
+        <!-- 表格 -->
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column fixed prop="date" label="Date" width="150" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="state" label="State" width="120" />
+            <el-table-column prop="city" label="City" width="120" />
+            <el-table-column prop="address" label="Address" width="600" />
+            <el-table-column prop="zip" label="Zip" width="120" />
+            <el-table-column fixed="right" label="Operations" width="120">
+                <template #default>
+                    <el-button link type="primary" size="small" @click="handleRowClick">Detail</el-button>
+                    <el-button link type="primary" size="small">Edit</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<style scoped>
+.table-box{
+    width:800px;
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+}
+</style>
+```
+
+![image-20221227110122274](https://cdn.jsdelivr.net/gh/ranyong1997/image_collect@main/img/202212271101898.png)
 
 
 
--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,89 @@
  * @version: 
  * @Author: 冉勇
  * @Date: 2022-12-27 09:50:33
- * @LastEditTime: 2022-12-27 10:21:23
+ * @LastEditTime: 2022-12-27 10:39:04
 -->
 <script setup>
+import { ref } from "vue";
+// 数据
+let queryInput = ref("")
+let tableData = ref([{
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+},
+{
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+},
+{
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+},
+{
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+}])
+// 方法
+const handleRowClick = () => {
+    console.log('click')
+}
 </script>
 
 <template>
-    <el-row class="mb-4">
-        <el-button>Default</el-button>
-        <el-button type="primary">Primary</el-button>
-        <el-button type="success">Success</el-button>
-        <el-button type="info">Info</el-button>
-        <el-button type="warning">Warning</el-button>
-        <el-button type="danger">Danger</el-button>
-    </el-row>
+    <div class="table-box">
+        <!-- 标题 -->
+        <div class="title">
+            <h2>CRUD</h2>
+        </div>
+        <!-- 功能 -->
+        <div class="query-box">
+            <el-input v-model="queryInput" placeholder="请输入姓名搜索" />
+            <el-button type="primary">增加</el-button>
+        </div>
+        <!-- 表格 -->
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column fixed prop="date" label="Date" width="150" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="state" label="State" width="120" />
+            <el-table-column prop="city" label="City" width="120" />
+            <el-table-column prop="address" label="Address" width="600" />
+            <el-table-column prop="zip" label="Zip" width="120" />
+            <el-table-column fixed="right" label="Operations" width="120">
+                <template #default>
+                    <el-button link type="primary" size="small" @click="handleRowClick">Detail</el-button>
+                    <el-button link type="primary" size="small">Edit</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <style scoped>
+.table-box{
+    width:800px;
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+}
 </style>
